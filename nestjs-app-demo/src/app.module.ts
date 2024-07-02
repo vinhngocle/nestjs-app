7135c6524b14771e56471574d2905b8c754b7d1c
Vinh Le <lengocvinh.729@gmail.com>
2024-07-02 14:32:31 +0700

    init project users-microservice and payments-microservice

--- nestjs-app-demo/src/app.module.ts
+++ nestjs-app-demo/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NATS_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://nats'],
+        },
+      },
+    ]),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
