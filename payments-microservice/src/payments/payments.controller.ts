07a77eb755953f59b4173147732c2042b3054fd0
Vinh Le <lengocvinh.729@gmail.com>
2024-07-03 15:02:20 +0700

    communication between microservices

--- payments-microservice/src/payments/payments.controller.ts
+++ payments-microservice/src/payments/payments.controller.ts
@@ -1,11 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy, EventPattern, Payload } from '@nestjs/microservices';
 import { CreatePaymentDto } from './dtos/CreatePayment.dto';
 
 @Controller('payments')
 export class PaymentsMicroserviceController {
+  constructor(@Inject('NATS_SERVICE') private nasClient: ClientProxy) {}
+
   @EventPattern('createPayment')
   createPayment(@Payload() createPaymentDto: CreatePaymentDto) {
     console.log(createPaymentDto);
+    this.nasClient.emit('paymentCreated', createPaymentDto);
   }
 }
