9889efd9c9403e896606c586fd8f6d4753954fb2
Vinh Le <lengocvinh.729@gmail.com>
2024-09-10 11:51:54 +0700

    add ui login, register

--- vite-reactjs-app/src/components/Login/Login.tsx
+++ vite-reactjs-app/src/components/Login/Login.tsx
@@ -4,9 +4,9 @@ function Login() {
   return (
     // <div className="w-full max-w-xs">
     <div className="flex flex-col justify-center items-center mt-[150px]">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-[32rem] border">
-        <h1 className="text-2xl text-center text-extrabold">Login</h1>
-        <div className="mb-4">
+      <form className="bg-white border shadow-md rounded px-8 pt-6 pb-8 mb-4 w-[30rem] md:w-[30rem]">
+        <h1 className="text-2xl text-center font-bold mb-[2rem]">Login</h1>
+        <div className="mb-4 pl-2 pr-2">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="email"
@@ -17,10 +17,10 @@ function Login() {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
             type="text"
-            placeholder="Email"
+            placeholder="Enter your email"
           />
         </div>
-        <div className="mb-6">
+        <div className="pl-2 pr-2">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor="password"
@@ -28,15 +28,21 @@ function Login() {
             Password
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
-            placeholder="******************"
+            placeholder="Enter your assword"
           />
         </div>
-        <div className="flex items-center justify-between">
+        <a
+          className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 text-right mt-[1.5rem] mb-[1.5rem] pl-2"
+          href="#"
+        >
+          Forgot Password?
+        </a>
+        <div className="flex items-center justify-between pl-2 pr-2">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full pl-2 pr-2"
             type="button"
           >
             Log In
@@ -50,25 +56,72 @@ function Login() {
               Sign up
             </a>
           </div> */}
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
         </div>
-        <div>
-          <div className="p-2 text-center">Or Sign in with</div>
-          <div className="flex justify-center ">
-            <div className="m-2">Google</div>
-            <div className="m-2">Facebook</div>
-            <div className="m-2">Twiter</div>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="px-2 bg-gray-100 text-gray-800">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 grid grid-cols-3 gap-3">
+            <div>
+              <a
+                href="#"
+                className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+              >
+                <img
+                  className="h-5 w-5"
+                  src="https://www.svgrepo.com/show/512120/facebook-176.svg"
+                  alt=""
+                />
+                Facebook
+              </a>
+            </div>
+            <div>
+              <a
+                href="#"
+                className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+              >
+                <img
+                  className="h-5 w-5"
+                  src="https://www.svgrepo.com/show/513008/twitter-154.svg"
+                  alt=""
+                />
+                Twitter
+              </a>
+            </div>
+            <div>
+              <a
+                href="#"
+                className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+              >
+                <img
+                  className="h-6 w-6"
+                  src="https://www.svgrepo.com/show/506498/google.svg"
+                  alt=""
+                />
+                Google
+              </a>
+            </div>
           </div>
         </div>
+        <div className="m-auto mt-6 w-fit md:mt-8">
+          <span className="m-auto dark:text-gray-400">
+            Don't have an account?
+            <a
+              className="font-semibold text-indigo-600 dark:text-indigo-100"
+              href="/register"
+            >
+              Create Account
+            </a>
+          </span>
+        </div>
       </form>
-      {/* <p className="text-center text-gray-500 text-xs">
-        &copy;2020 Acme Corp. All rights reserved.
-      </p> */}
     </div>
   );
 }
